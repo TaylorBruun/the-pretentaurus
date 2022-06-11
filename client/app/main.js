1ddde03bf14cbcd8620ee0e9f97ef1af3d2f0e97
Caleb Elliott <caleb123elliott@gmail.com>
2022-06-10 20:19:43 -0600

    asdf

--- client/app/main.js
+++ client/app/main.js
@@ -1,6 +1,7 @@
 import { AuthController } from './Controllers/AuthController.js'
 import { CommentsController } from './Controllers/CommentsController.js';
 import { PostsController } from './Controllers/PostsController.js';
+import { ThesaurusController } from "./Controllers/ThesaurusController.js";
 import { ValuesController } from './Controllers/ValuesController.js'
 
 class App {
@@ -8,6 +9,7 @@ class App {
   valuesController = new ValuesController();
   commentsController = new CommentsController()
   postsController = new PostsController()
+  thesaurusController = new ThesaurusController()
 }
 
 // @ts-ignore
